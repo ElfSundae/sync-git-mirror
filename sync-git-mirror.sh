f8f4923a4aae34e91eb1cd850ba00761589ca8e3
Elf Sundae <elf.sundae@gmail.com>
2020-08-25 16:15:33 +0800

    Add ref to my gist URL

--- sync-git-mirror.sh
+++ sync-git-mirror.sh
@@ -81,6 +81,7 @@ cd "$REPO_PATH"
 git fetch origin --prune --prune-tags
 
 # Checkout all remote branches
+# https://gist.github.com/ElfSundae/92a5868f418ec3187dfff90fe6b20387
 # https://stackoverflow.com/a/6300386/521946
 remote=origin ; for brname in `git branch -r | grep $remote | grep -v /master | grep -v /HEAD | awk '{gsub(/^[^\/]+\//,"",$1); print $1}'`; do git branch --track $brname $remote/$brname || true; done 2>/dev/null
 
